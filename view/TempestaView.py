0eb9820de6ac2c50f7c59c77e61cebeb7ba4af71
xaviercm94 <xaviercm@kth.se>
2020-03-21 18:36:45 +0100

    Console & Presets

--- view/TempestaView.py
+++ view/TempestaView.py
@@ -7,6 +7,11 @@ Created on Fri Mar 20 15:13:24 2020
 from pyqtgraph.Qt import QtGui
 import view.widgets as widgets
 import sys
+from pyqtgraph.console import ConsoleWidget
+import pyqtgraph as pg
+import numpy as np
+import os
+import view.guitools as guitools
 
 class TempestaView():
     
@@ -25,6 +30,7 @@ class TempestaView():
         
         # Menu Bar
         # TODO
+
         
         self.win.setWindowTitle('Tempesta 2.0')
         self.win.cwidget = QtGui.QWidget()
@@ -33,7 +39,26 @@ class TempestaView():
         layout = QtGui.QGridLayout()
         self.win.cwidget.setLayout(layout)
         
-        # Add all Widgets
+        # Presets
+        self.presetsMenu = QtGui.QComboBox()
+        self.controlFolder = os.path.split(os.path.realpath(__file__))[0]
+        os.chdir(self.controlFolder)
+        self.presetDir = os.path.join(self.controlFolder, 'presets')
+
+        if not os.path.exists(self.presetDir):
+            os.makedirs(self.presetDir)
+
+        for preset in os.listdir(self.presetDir):
+            self.presetsMenu.addItem(preset)
+        self.loadPresetButton = QtGui.QPushButton('Load preset')
+
+        #def loadPresetFunction(): return guitools.loadPreset(self)
+        #self.loadPresetButton.pressed.connect(loadPresetFunction)
+
+        layout.addWidget(self.presetsMenu, 0, 0)
+        layout.addWidget(self.loadPresetButton, 0, 1)
+        
+        # Add all other Widgets
         self.settingsWidget = widgets.SettingsWidget()
         layout.addWidget(self.settingsWidget, 1, 0, 2, 2)
         
@@ -45,6 +70,9 @@ class TempestaView():
         
         self.recordingWidget = widgets.RecordingWidget()
         layout.addWidget(self.recordingWidget, 4, 0, 1, 2)
+        
+        console = ConsoleWidget(namespace={'pg': pg, 'np': np})
+        layout.addWidget(console, 5, 0, 1, 2)
         # TODO
         layout.setRowMinimumHeight(2, 175)
         layout.setRowMinimumHeight(3, 100)

c788460614bf09fd50838c7b220ee610712fc8e3
xaviercm94 <xaviercm@kth.se>
2020-03-30 15:45:00 +0200

    controllers: laser & alignment tools

--- controller/MasterController.py
+++ controller/MasterController.py
@@ -6,9 +6,13 @@ Created on Tue Mar 24 16:41:57 2020
 """
 import numpy as np
 class MasterController():
+    
     def __init__(self):
         print('init master controller')
         self.stagePos = [0, 0, 0]
+    
+    def addVb(self, vb):
+        self.vb = vb
 
     def moveStage(self, axis, dist):
         self.stagePos[axis] += dist
@@ -18,11 +22,20 @@ class MasterController():
         im = 20 + np.zeros((20, 20))
         return im
         
-    def addItemTovb(self, plot):
-        print('Item added to viewbox')
+    def addItemTovb(self, item):
+        self.vb.addItem(item)
         
-    def removeItemFromvb(self, plot):
-        print('Item removed from viewbox')
+    def removeItemFromvb(self, item):
+        self.vb.removeItem(item)
         
     def getImageSize(self):
         return [500, 500]
+        
+    def toggleLaser(self, enable, laser):
+        print('Change enabler of laser '+ str(laser) + 'to ' + str(enable))
+        
+    def changePower(self, magnitude, laser):
+        print('Change power of laser '+ str(laser) + 'to ' + str(magnitude))
+        
+    def digitalMod(self, digital, powers, laser):
+        print('Digital modulation for laser '+ str(laser) + 'set to ' + str(digital))

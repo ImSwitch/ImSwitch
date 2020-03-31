6a6bfafbc2c0896e29ce9638f140575977c4e44e
xaviercm94 <xaviercm@kth.se>
2020-03-31 18:51:25 +0200

    Changes in addVb communication

--- controller/MasterController.py
+++ controller/MasterController.py
@@ -5,14 +5,25 @@ Created on Tue Mar 24 16:41:57 2020
 @author: _Xavi
 """
 import numpy as np
+
+#class CameraHelper():
+#    def __init__(self):
+#    # Should the recorder be another helper?
+#    def startAquisition(self):
+#        
+#    def stopAquisition(self):
+#        
+#    def changeParameter(self):
+#        
+#    def getLatestFrame(self):
+#        
+#    def getNewFrames(self):
+        
 class MasterController():
     
     def __init__(self):
         print('init master controller')
         self.stagePos = [0, 0, 0]
-    
-    def addVb(self, vb):
-        self.vb = vb
 
     def moveStage(self, axis, dist):
         self.stagePos[axis] += dist
@@ -21,12 +32,6 @@ class MasterController():
     def getImage(self):
         im = 20 + np.zeros((20, 20))
         return im
-        
-    def addItemTovb(self, item):
-        self.vb.addItem(item)
-        
-    def removeItemFromvb(self, item):
-        self.vb.removeItem(item)
         
     def getImageSize(self):
         return [500, 500]
@@ -39,3 +44,4 @@ class MasterController():
         
     def digitalMod(self, digital, powers, laser):
         print('Digital modulation for laser '+ str(laser) + ' set to ' + str(digital))
+        

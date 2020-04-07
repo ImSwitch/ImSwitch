f7727243427147bc57e41ba16f0c1ab6aac882bb
xaviercm94 <xaviercm@kth.se>
2020-04-07 16:47:56 +0200

    Style changes

---
+++ controller/NidaqHelper.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 16:46:33 2020
+
+@author: _Xavi
+"""
+
+#class NidaqHelper():
+#    
+#    def __init__(self, ):
+#        import nidaqmx
+#        self.deviceInfo = [['488 Exc', 1, [0, 247, 255]],
+#                           ['405', 2, [130, 0, 200]],
+#                           ['488 OFF', 3, [0, 247, 255]],
+#                           ['Camera', 4, [255, 255, 255]]]
+#    def __createDOTask(self):
+#    def __createAOTask(self):
+#        
+#    def setDigital(self, target, enable):
+#    def setAnalog(self, target, voltage):
+#
+#    def runScan(self, analog_targets, digital_targets, analog_signals, digital_signals):
+#    def runContinuous(self, digital_targets, digital_signals):
+
+        

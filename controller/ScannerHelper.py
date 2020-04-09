d5516d36d3894d58b6aa458df5d4d72459eeef34
Andreas   Bodén <andreas.boden@AD.SCILIFELAB.SE>
2020-04-09 09:59:10 +0200

    Finalized first version of ScanHelper with ScanDesigner

--- controller/ScannerHelper.py
+++ controller/ScannerHelper.py
@@ -5,169 +5,97 @@ Created on Tue Apr  7 17:00:05 2020
 @author: andreas.boden
 """
 
+
+from TempestaErrors import InvalidChildClassError, IncompatibilityError
+import SignalDesigner
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 class ScanHelper():
-    def __init__(self):
-        self.stageScanDesigner = StageScanDesigner()
-        self.ttlCycleDesigner = TTLCycleDesigner()
-
-class SignalDesigner():
-    def __init__(self):
+    def __init__(self, stageScaneParameters, TTLCycleParameters, syncParameters):
+        self.__stageScanDesigner = SignalDesigner.SignalDesignerFactory('stageScanDesigner', stageScaneParameters)
+        self.__ttlCycleDesigner = SignalDesigner.SignalDesignerFactory('TTLCycleDesigner', TTLCycleParameters)
         
-        self.last_signal = None
-        self.last_parameter_dict = None
+        self._expectedSyncParameters = ['TTLRepetitions', 'TTLZeroPad_seconds', 'Sample_rate']
         
-        self._expected_parameters = None
+        if not self._sync_parameter_compatibility(syncParameters):
+            print('WARNING: Incompatible sync parameters')
         
-    @property
-    def expected_parameters(self):
-        if self._expected_parameters is None:
-            raise ValueError('Value "%s" is not defined')
-        else:
-            return self._expected_parameters
-    
-
-    def make_signal(self, parameter_dict):
-        raise NotImplementedError("Method not implemented in child")
-    
-    def _parameter_compatibility(self, parameter_dict):
-        expected = set(self._expected_parameters)
+    def _sync_parameter_compatibility(self, parameter_dict):
+        expected = set(self._expectedSyncParameters)
         incoming = set([*parameter_dict])
         
         return expected == incoming
-    
         
-class StageScanDesigner(SignalDesigner):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def make_full_scan(self, stageScanParameters, TTLParameters, SyncParameters):
         
-        self._expected_parameters = ['Targets[3]',\
-                                     'Sizes[3]', \
-                                     'Step_sizes[3]', \
-                                     'Sequence_time_seconds', \
-                                     'Sample_rate', \
-                                     'Return_time_seconds']
+        stageScanSignalsDict = \
+        self.__stageScanDesigner.make_signal(stageScanParameters)
         
+        TTLCycleSignalsDict = \
+        self.__ttlCycleDesigner.make_signal(TTLParameters)
         
-        
-    def make_signal(self, parameter_dict):
-        
-        if not self._parameter_compatibility(parameter_dict):
-            print('WHAT ARE YOU DOING??? THESE PARAMETERS ARE WIEEEERD!?!?!?')
-            return None
-        #Retrieve sizes
-        [fast_axis_size, middle_axis_size, slow_axis_size] = \
-        [parameter_dict['Sizes[3]'][i] for i in range(3)]
-        
-        #Retrieve step sized
-        [fast_axis_step_size, middle_axis_step_size, slow_axis_step_size] = \
-        [parameter_dict['Step_sizes[3]'][i] for i in range(3)]
-        
-        fast_axis_positions =  1 + np.int(np.ceil(fast_axis_size / fast_axis_step_size))
-        middle_axis_positions = 1 + np.int(np.ceil(middle_axis_size / middle_axis_step_size))
-        slow_axis_positions = 1 + np.int(np.ceil(slow_axis_size / slow_axis_step_size))
-        
-        """
-        fast_axis_best_step_size = fast_axis_size / fast_axis_steps
-        middle_axis_best_step_size = middle_axis_size / middle_axis_steps
-        slow_axis_best_step_size = slow_axis_size / slow_axis_steps
-        """
-        sequenceSamples = \
-        parameter_dict['Sequence_time_seconds'] * parameter_dict['Sample_rate']
-        returnSamples = \
-        parameter_dict['Return_time_seconds'] * parameter_dict['Sample_rate']
-        if not sequenceSamples.is_integer():
-            print('WARNIGN: Non-integer number of sequence sampels, rounding up')
-            sequenceSamples = np.ceil(sequenceSamples)
-        if not returnSamples.is_integer():
+        #Calculate samples to zero pad TTL signals with
+        TTLZeroPadSamples = SyncParameters['TTLZeroPad_seconds'] * \
+        SyncParameters['Sample_rate']
+        if not TTLZeroPadSamples.is_integer():
             print('WARNIGN: Non-integer number of return sampels, rounding up')
-            returnSamples = np.ceil(returnSamples)
-        
-        
-        #Make fast axis signal
-        rampSamples = fast_axis_positions * sequenceSamples
-        lineSamples = rampSamples + returnSamples
-        rampSignal = self.makeRamp(0, fast_axis_size, rampSamples)
-        returnRamp = self.smoothRamp(fast_axis_size, 0, returnSamples)
-        fullLineSignal = np.concatenate((rampSignal, returnRamp))
-        
-        fastAxisSignal = np.tile(fullLineSignal, middle_axis_positions*slow_axis_positions)
-        plt.plot(fastAxisSignal)
-        
-        #Make middle axis signal
-        colSamples = middle_axis_positions * lineSamples
-        colValues = self.makeRamp(0, middle_axis_size, middle_axis_positions)
-        fullSquareSignal = np.zeros(colSamples)
-        for s in range(middle_axis_positions):
-            fullSquareSignal[s*lineSamples: \
-                             s*lineSamples + rampSamples] = \
-                             colValues[s]
-                             
-            try:
-                fullSquareSignal[s*lineSamples + rampSamples: \
-                                 (s+1)*lineSamples] = \
-                                 self.smoothRamp(colValues[s], colValues[s+1], returnSamples)
-            except IndexError:
-                fullSquareSignal[s*lineSamples + rampSamples: \
-                                 (s+1)*lineSamples] = \
-                                 self.smoothRamp(colValues[s], 0, returnSamples)
-        
-        middleAxisSignal = np.tile(fullSquareSignal, slow_axis_positions)
-        plt.plot(middleAxisSignal)
-        
-        #Make slow axis signal
-        sliceSamples = slow_axis_positions * colSamples
-        sliceValues = self.makeRamp(0, slow_axis_size, slow_axis_positions)
-        fullCubeSignal = np.zeros(sliceSamples)
-        for s in range(slow_axis_positions):
-            fullCubeSignal[s*colSamples: \
-                             (s+1)*colSamples - returnSamples] = \
-                             sliceValues[s]
-                             
-            try:
-                fullCubeSignal[(s+1)*colSamples - returnSamples: \
-                                 (s+1)*colSamples] = \
-                                 self.smoothRamp(sliceValues[s], sliceValues[s+1], returnSamples)
-            except IndexError:
-                fullCubeSignal[(s+1)*colSamples - returnSamples: \
-                                 (s+1)*colSamples] = \
-                                 self.smoothRamp(sliceValues[s], 0, returnSamples)
-        slowAxisSignal = fullCubeSignal             
-        plt.plot(slowAxisSignal)
-        
-        sig_dict = {parameter_dict['Targets[3]'][0]: fastAxisSignal, \
-                    parameter_dict['Targets[3]'][1]: middleAxisSignal, \
-                    parameter_dict['Targets[3]'][2]: slowAxisSignal}
-        
-        return sig_dict
-        
-    def makeRamp(self, start, end, samples):
-        return np.linspace(start, end, num=samples)
+            TTLZeroPadSamples = np.ceil(TTLZeroPadSamples)
+        #Tile and pad TTL signals according to sync parameters
+        for target, signal in TTLCycleSignalsDict.items():
+            signal = np.tile(signal, SyncParameters['TTLRepetitions'])
+            signal = np.append(signal, np.zeros(TTLZeroPadSamples))
+            TTLCycleSignalsDict[target] = signal
+            
+        return [stageScanSignalsDict, TTLCycleSignalsDict]
 
 
-    def smoothRamp(self, start, end, samples):
-        curve_half = 0.6
-        x = np.linspace(0, np.pi/2, num=np.floor(curve_half*samples), endpoint=True)
-        signal = start + (end-start)*np.sin(x)
-        signal = np.append(signal, end*np.ones(int(np.ceil((1-curve_half)*samples))))
-        return signal
-    
-    
-    
+            
 if __name__ == '__main__':
     print('Running main')
-    ssd = StageScanDesigner()
+
+    Stageparameters = {'Targets[3]': ['StageX', 'StageY', 'StageZ'], \
+                  'Sizes[3]':[5,5,0], \
+                  'Step_sizes[3]': [1,1,1], \
+                  'Sequence_time_seconds': 0.005, \
+                  'Sample_rate': 100000, \
+                  'Return_time_seconds': 0.001}
+    TTLparameters = {'Targets[x]': ['405', '488'], \
+                  'TTLStarts[x,y]': [[0.0012, 0.002], [0, 0]], \
+                  'TTLEnds[x,y]': [[0.0015, 0.0025], [0, 0]], \
+                  'Sequence_time_seconds': 0.005, \
+                  'Sample_rate': 100000}
+    
+    SyncParameters = {'TTLRepetitions': 6, 'TTLZeroPad_seconds': 0.001, 'Sample_rate': 100000}
+    sh = ScanHelper(Stageparameters, TTLparameters, SyncParameters)
+    ttlsig = sh.make_full_scan(Stageparameters, TTLparameters, SyncParameters)
+
+    """
     parameters = {'Targets[3]': ['StageX', 'StageY', 'StageZ'], \
                   'Sizes[3]':[5,5,5], \
                   'Step_sizes[3]': [1,1,1], \
                   'Sequence_time_seconds': 0.005, \
                   'Sample_rate': 100000, \
                   'Return_time_seconds': 0.001}
-            
+    ssd = SignalDesignerFactory('stageScanDesigner', parameters)        
     sig_dict = ssd.make_signal(parameters)
     
+    parameters = {'Targets[x]': ['405', '488'], \
+                  'TTLStarts[x,y]': [[0.0012, 0.002], [0, 0]], \
+                  'TTLEnds[x,y]': [[0.0015, 0.0025], [0, 0]], \
+                  'Sequence_time_seconds': 0.005, \
+                  'Sample_rate': 100000}
+    ttldesigner = SignalDesignerFactory('TTLCycleDesigner', parameters)
+    ttlDict = ttldesigner.make_signal(parameters)
+    """
+    """
+    d = {'hej': 1, 'då': 2}
     
+    for key, value in d.items():
+        print(key)
+        value = 3
+        print(value)
+   """ 
+        
+        
     

9c4cd67579b302650899517f1aa14d084b692aa6
Andreas   Bodén <andreas.boden@AD.SCILIFELAB.SE>
2020-04-09 09:59:10 +0200

    Finalized first version of ScanHelper with ScanDesigner

---
+++ controller/TempestaErrors.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 19:10:01 2020
+
+@author: andreas.boden
+"""
+
+
+
+class InvalidChildClassError(Exception):
+    """Exception raised when trying to inialize an invalid child"""
+    def __init__(self, message):
+        self.message = message
+        
+class IncompatibilityError(Exception):
+    """Exception raised when trying to inialize an invalid child"""
+    def __init__(self, message):
+        self.message = message 

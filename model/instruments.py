45b0cf1ff13b920c352280d4203e64fb7220f711
stafak <stafak@kth.se>
2020-10-22 15:33:38 +0200

    Remove hard-coding of 2 lasers in LinkedLaser

--- model/instruments.py
+++ model/instruments.py
@@ -31,11 +31,14 @@ class LinkedLaserCheck:
             pName, driverName = iName.rsplit('.', 1)
             package = importlib.import_module('lantz.drivers.legacy.' + pName)
             driver = getattr(package, driverName)
-            laser0 = driver(ports[0])
-            laser0.initialize()
-            laser1 = driver(ports[1])
-            laser1.initialize()
-            return LinkedLaser([laser0, laser1])
+
+            lasers = []
+            for port in ports:
+                laser = driver(port)
+                laser.initialize()
+                lasers = lasers.append(laser)
+
+            return LinkedLaser(lasers)
 
         except:
             return mockers.MockLaser()
@@ -44,74 +47,103 @@ class LinkedLaserCheck:
 class LinkedLaser:
 
     def __init__(self, lasers):
+        if len(lasers) < 1:
+            raise ValueError('LinkedLaser requires at least one laser, none passed')
+
         self.lasers = lasers
 
     @property
     def idn(self):
-        return 'Linked Lasers' + self.lasers[0].idn + self.lasers[1].idn
+        return 'Linked Lasers ' + ' '.join([laser.idn for laser in self.lasers])
 
     @property
     def autostart(self):
-        return self.lasers[0].autostart
+        value = self.lasers[0].autostart
+        for laser in self.lasers:
+            if laser.autostart != value:
+                raise ValueError(f'Laser {laser.idn} autostart state is {laser.autostart} while'
+                                 f' laser {self.lasers[0]} autostart state is {value}')
+
+        return value
 
     @autostart.setter
     def autostart(self, value):
-        self.lasers[0].autostart = self.lasers[1].autostart = value
+        for laser in self.lasers:
+            laser.autostart = value
 
     @property
     def enabled(self):
-        return self.lasers[0].enabled
+        value = self.lasers[0].enabled
+        for laser in self.lasers:
+            if laser.enabled != value:
+                raise ValueError(f'Laser {laser.idn} enabled state is {laser.enabled} while laser'
+                                 f' {self.lasers[0]} enabled state is {value}')
+
+        return value
 
     @enabled.setter
     def enabled(self, value):
-        self.lasers[0].enabled = self.lasers[1].enabled = value
+        for laser in self.lasers:
+            laser.enabled = value
 
     @property
     def power(self):
-        return self.lasers[0].power + self.lasers[1].power
+        return sum([laser.power for laser in self.lasers])
 
     @property
     def power_sp(self):
-        return self.lasers[0].power_sp + self.lasers[1].power_sp
+        return sum([laser.power_sp for laser in self.lasers])
 
     @power_sp.setter
     def power_sp(self, value):
-        self.lasers[0].power_sp = self.lasers[1].power_sp = 0.5*value
+        for laser in self.lasers:
+            laser.power_sp = value / len(self.lasers)
 
     @property
     def digital_mod(self):
-        return self.lasers[0].digital_mod
+        value = self.lasers[0].digital_mod
+        for laser in self.lasers:
+            if laser.digital_mod != value:
+                raise ValueError(f'Laser {laser.idn} digital_mod state is {laser.digital_mod} while'
+                                 f' laser {self.lasers[0]} digital_mod state is {value}')
+
+        return value
 
     @digital_mod.setter
     def digital_mod(self, value):
-        self.lasers[0].digital_mod = self.lasers[1].digital_mod = value
+        for laser in self.lasers:
+            laser.digital_mod = value
 
     @property
     def mod_mode(self):
-        return [self.lasers[0].mod_mode, self.lasers[1].mod_mode]
+        return [laser.mod_mode for laser in self.lasers]
 
     def enter_mod_mode(self):
-        [self.lasers[i].enter_mod_mode() for i in [0, 1]]
+        for laser in self.lasers:
+            laser.enter_mod_mode()
 
     def changeEdit(self):
-        [self.lasers[i].changeEdit() for i in [0, 1]]
+        for laser in self.lasers:
+            laser.changeEdit()
 
     def query(self, value):
-        [self.lasers[i].query(value) for i in [0, 1]]
+        for laser in self.lasers:
+            laser.query(value)
 
     @property
     def power_mod(self):
         """Laser modulated power (mW).
         """
-        return self.lasers[0].power_mod + self.lasers[1].power_mod
+        return sum([laser.power_mod for laser in self.lasers])
 
     @power_mod.setter
     def power_mod(self, value):
-        self.lasers[0].power_mod = self.lasers[1].power_mod = 0.5*value
+        for laser in self.lasers:
+            laser.power_mod = value / len(self.lasers)
 
     def finalize(self):
-        self.lasers[0].finalize()
-        self.lasers[1].finalize()
+        for laser in self.lasers:
+            laser.finalize()
 
 class Cameras:
     """ Buffer class for testing whether the camera is connected. If it's not,

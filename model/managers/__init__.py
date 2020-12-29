1f2bcd888a3c8cfbaff5ef6aa3f54bec7b80a6d3
stafak <stafak@kth.se>
2020-12-29 17:27:01 +0100

    Use a PositionerManager for abstraction of positioners

--- model/managers/__init__.py
+++ model/managers/__init__.py
@@ -1,5 +1,6 @@
 from .DetectorsManager import DetectorsManager, NoDetectorsError
 from .LaserManager import LaserManager
 from .NidaqManager import NidaqManager
+from .PositionersManager import PositionersManager
 from .RecordingManager import RecordingManager
 from .ScanManager import ScanManager

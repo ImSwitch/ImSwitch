010178db478d1f4fe6ced84816349980973df5ec
jonatanalvelid <35332627+jonatanalvelid@users.noreply.github.com>
2021-01-14 17:55:44 +0100

    apdmanager creation

--- controller/controllers/hardwarecontrollers.py
+++ controller/controllers/hardwarecontrollers.py
@@ -376,3 +376,105 @@ class BeadWorker(Worker):
                     if i == N:
                         i = 0
                 self.newChunk.emit()
+
+
+class MotCorrController(WidgetController):
+    """ Linked to MotCorrWidget."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self._manager = self._master.rs232sManager._subManagers['leicadmi']
+        #TODO: BELOW IS JUST COPIED FROM LASERCONTROLLER
+        self.aotfLasers = {}
+        for laserName, laserManager in self._master.lasersManager:
+            if not laserManager.isDigital:
+                self.aotfLasers[laserName] = False
+
+        # Connect LaserWidget signals
+        for laserModule in self._widget.laserModules.values():
+            if not self._master.lasersManager[laserModule.laser].isBinary:
+                if self._master.lasersManager[laserModule.laser].isDigital:
+                    self.changeEdit(laserModule.laser)
+
+                laserModule.slider.valueChanged[int].connect(lambda _, laser=laserModule.laser: self.changeSlider(laser))
+                laserModule.setPointEdit.returnPressed.connect(lambda laser=laserModule.laser: self.changeEdit(laser))
+
+            laserModule.enableButton.toggled.connect(lambda _, laser=laserModule.laser: self.toggleLaser(laser))
+
+        for digModuleLaser in self._widget.digModule.powers.keys():
+            self._widget.digModule.powers[digModuleLaser].textChanged.connect(
+                lambda _, laser=digModuleLaser: self.updateDigitalPowers([laser])
+            )
+
+        self._widget.digModule.DigitalControlButton.clicked.connect(
+            lambda: self.GlobalDigitalMod(list(self._widget.digModule.powers.keys()))
+        )
+        self._widget.digModule.updateDigPowersButton.clicked.connect(
+            lambda: self.updateDigitalPowers(list(self._widget.digModule.powers.keys()))
+        )
+
+    def closeEvent(self):
+        self._master.lasersManager.execOnAll(lambda l: l.setDigitalMod(False, 0))
+        self._master.lasersManager.execOnAll(lambda l: l.setValue(0))
+
+    def toggleLaser(self, laserName):
+        """ Enable or disable laser (on/off)."""
+        self._master.lasersManager[laserName].setEnabled(
+            self._widget.laserModules[laserName].enableButton.isChecked()
+        )
+
+    def changeSlider(self, laserName):
+        """ Change power with slider magnitude. """
+        magnitude = self._widget.laserModules[laserName].slider.value()
+        if laserName not in self.aotfLasers.keys() or not self.aotfLasers[laserName]:
+            self._master.lasersManager[laserName].setValue(magnitude)
+            self._widget.laserModules[laserName].setPointEdit.setText(str(magnitude))
+
+    def changeEdit(self, laserName):
+        """ Change power with edit magnitude. """
+        magnitude = float(self._widget.laserModules[laserName].setPointEdit.text())
+        if laserName not in self.aotfLasers.keys() or not self.aotfLasers[laserName]:
+            self._master.lasersManager[laserName].setValue(magnitude)
+            self._widget.laserModules[laserName].slider.setValue(magnitude)
+
+    def updateDigitalPowers(self, laserNames):
+        """ Update the powers if the digital mod is on. """
+        if self._widget.digModule.DigitalControlButton.isChecked():
+            for laserName in laserNames:
+                self._master.lasersManager[laserName].setValue(
+                    float(self._widget.digModule.powers[laserName].text())
+                )
+
+    def GlobalDigitalMod(self, laserNames):
+        """ Start digital modulation. """
+        digMod = self._widget.digModule.DigitalControlButton.isChecked()
+        for laserName in laserNames:
+            laserModule = self._widget.laserModules[laserName]
+            laserManager = self._master.lasersManager[laserName]
+
+            if laserManager.isBinary:
+                continue
+
+            value = float(self._widget.digModule.powers[laserName].text())
+            if laserManager.isDigital:
+                laserManager.setDigitalMod(digMod, value)
+            else:
+                laserManager.setValue(value)
+                laserModule.enableButton.setChecked(False)
+                laserModule.enableButton.setEnabled(not digMod)
+                self.aotfLasers[laserName] = digMod
+                laserManager.setEnabled(False)  # TODO: Correct?
+
+            laserModule.setPointEdit.setEnabled(not digMod)
+            laserModule.slider.setEnabled(not digMod)
+
+            if not digMod:
+                self.changeEdit(laserName)
+
+    def setDigitalButton(self, b):
+        self._widget.digModule.DigitalControlButton.setChecked(b)
+        self.GlobalDigitalMod(
+            [laser.name for laser in self._master.lasersManager if laser.isDigital]
+        )
+
---
+++ model/managers/detectors/APDManager.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 08 14:00:00 2021
+
+@author: jonatanalvelid
+"""
+
+import numpy as np
+
+from .DetectorManager import (
+    DetectorManager, DetectorNumberParameter, DetectorListParameter
+)
+from framework import Signal, SignalInterface, Thread, Worker
+
+class APDManager(DetectorManager):
+
+    # TODO: use the same manager for the PMT, with the type of detector as an argument. NidaqPointDetectorManager
+    def __init__(self, nidaqManager, name, **_kwargs):
+        model = 0
+        self.__name = name
+        self._image = np.array([])
+
+        for propertyName, propertyValue in webcamInfo.managerProperties['tis'].items():
+            self._camera.setPropertyValue(propertyName, propertyValue)
+
+        fullShape = (0, 0)
+
+        # Prepare parameters
+        parameters = {}
+        self._nidaqManager = nidaqManager
+        self._nidaqManager.scanStartSignal.connect(lambda nL, nP: self.startScan(nL, nP))
+        super().__init__(name, fullShape, [1], model, parameters)
+
+    def startScan(self, nLines, nPixels):
+        if self.acquisition:
+            self._scanWorker = ScanWorker(self, nLines, nPixels)
+            self._scanThread = Thread()
+            self._scanWorker.moveToThread(self._scanThread)
+            self._scanThread.started.connect(self._scanWorker.run)
+            self._scanWorker.scanning = True
+            self._scanWorker.newLine.connect(lambda line: self.updateImage(line))
+            self._scanThread.start()
+    
+    def startAcquisition(self):
+        self.acquisition = True
+
+    def stopAcquisition(self):
+        self._scanWorker.scanning = False
+        self._scanThread.quit()
+        self._scanThread.wait()
+    
+
+    def getLatestFrame(self):
+        return self._image
+
+    def updateImage(self, line):
+        print('update image')
+
+    def setParameter(self, name, value):
+        pass
+
+    def getParameter(self, name):
+       pass
+
+    def setBinning(self, binning):
+        super().setBinning(binning)
+    
+    def getChunk(self):
+        pass
+
+    def flushBuffers(self):
+        pass
+
+    def pixelSize(self):
+        pass
+
+    def crop(self, hpos, vpos, hsize, vsize):
+        pass
+
+    def show_dialog(self):
+        "Manager: open camera settings dialog mock."
+        pass
+
+class ScanWorker(Worker):
+    newLine = Signal(np.ndarray)
+    def __init__(self, manager, nLines, nPixels):
+        super().__init__()
+        self._name = manager.__name
+        manager._nidaqManager.startInputTask(self.name, 'ci')
+        self._manager = manager
+        self._nLines = nLines
+        self._nPixels = nPixels
+
+    def run(self):
+        lineCounter = 0
+        lastValue = 0
+        while lineCounter < self._nLines:
+            if self.scanning:
+                data = self._manager._nidaqManager.readInputTask(self._name, self._nPixels)
+                # galvo-sensor-data reading
+                subtractionArray = np.concatenate(([lastValue],data[:-1]))
+                lastValue = data[-1]
+                data = data - subtractionArray  # Now apd_data is an array contains at each position the number of counts at this position
+                line = np.absolute(data[0:len(data) // 2])
+                # TODO: Line_from_sine from old code, probably have to read galvo-sensor-data
+                newLine.emit(line)
+                lineCounter = lineCounter + 1
+            else:
+                self.close()
+        
+    def close(self):
+        self.manager.nidaqManager.stopInputTask(self._name)

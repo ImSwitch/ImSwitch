04b925ecbae0cc524348bcf55057136ba4c23bb3
stafak <stafak@kth.se>
2021-02-23 22:23:05 +0100

    Remove use of PyQt directly in imcontrol controller

--- imcommon/view/guitools/dialogtools.py
+++ imcommon/view/guitools/dialogtools.py
@@ -13,3 +13,21 @@ def askForTextInput(widget, title, label):
     clicked, None otherwise. """
     result, okClicked = QtWidgets.QInputDialog.getText(widget, title, label)
     return result if okClicked else None
+
+
+def askForFilePath(widget, caption=None, defaultFolder=None, nameFilter=None, isSaving=False):
+    """ Asks the user to pick a file path. Returns the file path if "OK" is
+    clicked, None otherwise. """
+    func = (QtWidgets.QFileDialog().getOpenFileName if not isSaving
+            else QtWidgets.QFileDialog().getSaveFileName)
+
+    result = func(widget, caption=caption, directory=defaultFolder, filter=nameFilter)[0]
+    return result if result else None
+
+
+def askForFolderPath(widget, caption=None, defaultFolder=None):
+    """ Asks the user to pick a folder path. Returns the folder path if "OK" is
+    clicked, None otherwise. """
+    result = QtWidgets.QFileDialog.getExistingDirectory(widget, caption=caption,
+                                                        directory=defaultFolder)
+    return result if result else None

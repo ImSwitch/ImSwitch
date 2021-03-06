c988efcab0f589bac7e0bbf0738135d6bed28be1
xaviercm94 <xaviercm@kth.se>
2021-03-06 02:08:04 +0100

    Licenses

--- imcontrol/controller/controllers/RecorderController.py
+++ imcontrol/controller/controllers/RecorderController.py
@@ -228,3 +228,18 @@ class RecorderController(ImConWidgetController):
             attrs.update({'Rec:Slices': self._widget.getDimlapseSlices(),
                           'Rec:StepSize': self._widget.getDimlapseStepSize()})
         return attrs
+    
+# Copyright (C) 2020, 2021 Staffan Al-Kadhimi, Xavier Casas, Andreas Boden
+# This file is part of ImSwitch.
+#
+# ImSwitch is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ImSwitch is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
--- imcontrol/controller/controllers/ViewController.py
+++ imcontrol/controller/controllers/ViewController.py
@@ -46,3 +46,18 @@ class ViewController(ImConWidgetController):
     def closeEvent(self):
         if self._acqHandle is not None:
             self._master.detectorsManager.stopAcquisition(self._acqHandle, liveView=True)
+
+# Copyright (C) 2020, 2021 Staffan Al-Kadhimi, Xavier Casas, Andreas Boden
+# This file is part of ImSwitch.
+#
+# ImSwitch is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ImSwitch is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License

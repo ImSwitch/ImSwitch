0aa7a1056daf5faa3cbecebfc10df62c10c710a8
xaviercm94 <xaviercm@kth.se>
2021-03-06 01:14:39 +0100

    Add license notices and copyright

--- imcontrol/model/managers/positioners/PositionerManager.py
+++ imcontrol/model/managers/positioners/PositionerManager.py
@@ -26,3 +26,18 @@ class PositionerManager(ABC):
         """Adjusts the positioner to the specified position and returns the new
         position. Derived classes will update the position field manually."""
         pass
+
+# Copyright (C) 2020, 2021 Staffan Al-Kadhimi, Xavier Casas, Andreas Boden
+# This file is part of ImSwitch.
+#
+# ImSwitch is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ImSwitch is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License

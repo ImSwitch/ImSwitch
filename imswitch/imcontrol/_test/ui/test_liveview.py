3251f42c599c4c4e6618c4f5b66916e9df7f9a74
stafak <stafak@kth.se>
2021-03-30 12:50:12 +0200

    Add a few pytest tests

---
+++ imswitch/imcontrol/_test/ui/test_liveview.py
@@ -0,0 +1,61 @@
+import pytest
+from PyQt5 import QtCore, QtTest
+from . import prepareUI
+
+
+app, mainView = prepareUI()
+
+
+@pytest.fixture(scope='session')
+def qapp():
+    yield app
+
+
+def test_liveview_no_error(qtbot):
+    qtbot.mouseClick(mainView.viewWidget.liveviewButton, QtCore.Qt.LeftButton)
+    assert mainView.viewWidget.liveviewButton.isChecked()
+
+    QtTest.QTest.qWait(3000)
+    qtbot.mouseClick(mainView.viewWidget.liveviewButton, QtCore.Qt.LeftButton)
+    assert not mainView.viewWidget.liveviewButton.isChecked()
+
+
+def test_grid_no_error(qtbot):
+    qtbot.mouseClick(mainView.viewWidget.gridButton, QtCore.Qt.LeftButton)
+    assert mainView.viewWidget.gridButton.isChecked()
+
+    QtTest.QTest.qWait(100)
+    qtbot.mouseClick(mainView.viewWidget.gridButton, QtCore.Qt.LeftButton)
+    assert not mainView.viewWidget.gridButton.isChecked()
+
+
+def test_crosshair_no_error(qtbot):
+    qtbot.mouseClick(mainView.viewWidget.crosshairButton, QtCore.Qt.LeftButton)
+    assert mainView.viewWidget.crosshairButton.isChecked()
+
+    QtTest.QTest.qWait(100)
+    qtbot.mouseMove(mainView.imageWidget, pos=QtCore.QPoint(120, 120))
+    QtTest.QTest.qWait(100)
+    qtbot.mouseMove(mainView.imageWidget, pos=QtCore.QPoint(150, 160))
+
+    QtTest.QTest.qWait(100)
+    qtbot.mouseClick(mainView.viewWidget.crosshairButton, QtCore.Qt.LeftButton)
+    assert not mainView.viewWidget.crosshairButton.isChecked()
+
+
+
+# Copyright (C) 2020, 2021 TestaLab
+# This file is part of ImSwitch.
+#
+# ImSwitch is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ImSwitch is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
---
+++ imswitch/imcontrol/_test/unit/__init__.py
@@ -0,0 +1,96 @@
+from imswitch.imcontrol.model import DetectorInfo
+
+
+detectorInfosBasic = {
+    'CAM': DetectorInfo(
+        analogChannel=None,
+        digitalLine=3,
+        managerName='HamamatsuManager',
+        managerProperties={
+            'cameraListIndex': 0,
+            'hamamatsu': {
+                'readout_speed': 3,
+                'trigger_global_exposure': 5,
+                'trigger_active': 2,
+                'trigger_polarity': 2,
+                'exposure_time': 0.01,
+                'trigger_source': 1,
+                'subarray_hpos': 0,
+                'subarray_vpos': 0,
+                'subarray_hsize': 2048,
+                'subarray_vsize': 2048,
+                'image_width': 2048,
+                'image_height': 2048
+            }
+        }
+    )
+}
+
+detectorInfosMulti = {
+    'Camera 1': detectorInfosBasic['CAM'],
+    'Camera 2': DetectorInfo(
+        analogChannel=None,
+        digitalLine=5,
+        managerName='HamamatsuManager',
+        managerProperties={
+            'cameraListIndex': 0,
+            'hamamatsu': {
+                'readout_speed': 3,
+                'trigger_global_exposure': 5,
+                'trigger_active': 2,
+                'trigger_polarity': 2,
+                'exposure_time': 0.01,
+                'trigger_source': 1,
+                'subarray_hpos': 0,
+                'subarray_vpos': 0,
+                'subarray_hsize': 1024,
+                'subarray_vsize': 1024,
+                'image_width': 1024,
+                'image_height': 1024
+            }
+        }
+    )
+}
+
+
+detectorInfosNonSquare = {
+    'CAM': DetectorInfo(
+        analogChannel=None,
+        digitalLine=3,
+        managerName='HamamatsuManager',
+        managerProperties={
+            'cameraListIndex': 0,
+            'hamamatsu': {
+                'readout_speed': 3,
+                'trigger_global_exposure': 5,
+                'trigger_active': 2,
+                'trigger_polarity': 2,
+                'exposure_time': 0.01,
+                'trigger_source': 1,
+                'subarray_hpos': 0,
+                'subarray_vpos': 0,
+                'subarray_hsize': 2048,
+                'subarray_vsize': 1234,
+                'image_width': 2048,
+                'image_height': 1234
+            }
+        }
+    )
+}
+
+
+# Copyright (C) 2020, 2021 TestaLab
+# This file is part of ImSwitch.
+#
+# ImSwitch is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ImSwitch is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.

1b48d665f2de1b608db4b947e1cb71ad1f0d063a
stafak <stafak@kth.se>
2021-07-26 16:17:57 +0200

    Fix FFT not using correct starting image limits

--- imswitch/imcontrol/view/widgets/FFTWidget.py
+++ imswitch/imcontrol/view/widgets/FFTWidget.py
@@ -98,7 +98,7 @@ class FFTWidget(Widget):
         self.img.setImage(im, autoLevels=False)
 
     def updateImageLimits(self, imgWidth, imgHeight):
-        guitools.setBestPyQtGraphImageLimits(self.vb, imgWidth, imgHeight)
+        guitools.setPGBestImageLimits(self.vb, imgWidth, imgHeight)
 
     def getImageDisplayLevels(self):
         return self.hist.getLevels()

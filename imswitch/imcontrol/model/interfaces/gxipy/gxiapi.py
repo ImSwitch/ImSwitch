825090f838f03507c356a27dd0e59a0e0b083f35
edabngl <bene.d@gmx.de>
2021-11-17 14:02:46 +0100

    Get Doheng finally work

--- imswitch/imcontrol/model/interfaces/gxipy/gxiapi.py
+++ imswitch/imcontrol/model/interfaces/gxipy/gxiapi.py
@@ -3,9 +3,9 @@
 # -*-mode:python ; tab-width:4 -*- ex:set tabstop=4 shiftwidth=4 expandtab: -*-
 
 import numpy
-from gxipy.gxwrapper import *
-from gxipy.dxwrapper import *
-from gxipy.gxidef import *
+from .gxwrapper import *
+from .dxwrapper import *
+from .gxidef import *
 
 ERROR_SIZE = 1024
 PIXEL_BIT_MASK = 0x00ff0000

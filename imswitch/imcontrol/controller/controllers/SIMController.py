62ebda084e16a80acbc40ed4037387f7b1c57610
beniroquai <bene.d@gmx.de>
2023-01-08 23:47:39 +0100

    Starting to integrate mcSIM into ImSwitch realtime reconstruction

--- imswitch/imcontrol/controller/controllers/SIMController.py
+++ imswitch/imcontrol/controller/controllers/SIMController.py
@@ -12,6 +12,21 @@ from imswitch.imcommon.framework import Signal, Thread, Worker, Mutex, Timer
 
 from ..basecontrollers import LiveUpdatedController
 
+import os
+import time
+import numpy as np
+from pathlib import Path
+import tifffile
+try:
+    import cupy as cp
+    from mcsim.analysis import sim_reconstruction as sim
+    isGPU = True
+except:
+    print("GPU not available"")
+    import numpy as cp 
+    from mcsim.analysis import sim_reconstruction_cpu as sim
+    isGPU = False
+
 try:
     import NanoImagingPack as nip
     isNIP = True
@@ -37,6 +52,8 @@ class SIMController(LiveUpdatedController):
         self.__logger = initLogger(self)
         self.it=0
         self.updateRate=5
+        
+        self.reconstructionMethod = "napari" # or "mcSIM"
 
 
         self.simDir = os.path.join(dirtools.UserFileDirs.Root, 'imcontrol_sim')
@@ -107,8 +124,6 @@ class SIMController(LiveUpdatedController):
         for im_number, im in enumerate(allPatterns):
             self.hamamatsuslm.send_dat(im, im_number)
 
-
-
     def __del__(self):
         self.imageComputationThread.quit()
         self.imageComputationThread.wait()
@@ -124,7 +139,6 @@ class SIMController(LiveUpdatedController):
 
     def displayMask(self, image):
         self._widget.updateSIMDisplay(image)
-
 
     def SIMAcquisition(self):
         """ Update with new detector frame. """
@@ -317,6 +331,10 @@ class SIMProcessor(object):
         self.isCalibrated = False
         self.use_phases =  True
         self.use_torch = False
+        
+        
+        # switch for the different reconstruction algorithms
+        self.reconstructionMethod = "napari"
 
         '''
         initialize
@@ -339,7 +357,17 @@ class SIMProcessor(object):
         self.ky_input = np.zeros(self.k_shape, dtype=np.single)
         self.p_input = np.zeros(self.k_shape, dtype=np.single)
         self.ampl_input = np.zeros(self.k_shape, dtype=np.single)
+        
+        # set up the GPU for mcSIM
+        if isGPU:
+            # GPU memory usage
+            mempool = cp.get_default_memory_pool()
+            pinned_mempool = cp.get_default_pinned_memory_pool()
+            memory_start = mempool.used_bytes()
 
+
+    def setReconstructionMethod(self, method):
+        self.reconstructionMethod = method
 
     def setReconstructor(self):
         '''
@@ -388,38 +416,124 @@ class SIMProcessor(object):
         calibration
         '''
 
-        #imRaw = get_current_stack_for_calibration(mImages)
-        if self.use_torch:
-            self.h.calibrate_pytorch(imRaw,self.find_carrier)
-        else:
-            self.h.calibrate(imRaw,self.find_carrier)
-        self.isCalibrated = True
-        if self.find_carrier: # store the value found
-            self.kx_input = self.h.kx
-            self.ky_input = self.h.ky
-            self.p_input = self.h.p
-            self.ampl_input = self.h.ampl
+        if self.reconstructionMethod == "napari":
+            #imRaw = get_current_stack_for_calibration(mImages)
+            if self.use_torch:
+                self.h.calibrate_pytorch(imRaw, self.find_carrier)
+            else:
+                self.h.calibrate(imRaw, self.find_carrier)
+            self.isCalibrated = True
+            if self.find_carrier: # store the value found
+                self.kx_input = self.h.kx
+                self.ky_input = self.h.ky
+                self.p_input = self.h.p
+                self.ampl_input = self.h.ampl
+                
+        elif self.reconstructionMethod == "mcSIM":
+            """
+            test running SIM reconstruction at full speed on GPU
+            """
+                        # physical parameters
+            na = 1.3 # numerical aperture
+            dxy = 0.065 # um
+            wavelength = 0.488 # um
+
+
+            # ############################
+            # for the first image, estimate the SIM parameters
+            # this step is slow, can take ~1-2 minutes
+            # ############################
+            print("running initial reconstruction with full parameter estimation")
+            imgset = sim.SimImageSet({"pixel_size": dxy,
+                                    "na": na,
+                                    "wavelength": wavelength},
+                                    imRaw,
+                                    otf=None,
+                                    wiener_parameter=0.3,
+                                    frq_estimation_mode="band-correlation",
+                                    # frq_guess=frqs_gt, # todo: can add frequency guesses for more reliable fitting
+                                    phase_estimation_mode="wicker-iterative",
+                                    phases_guess=np.array([[0, 2*np.pi / 3, 4 * np.pi / 3],
+                                                            [0, 2*np.pi / 3, 4 * np.pi / 3],
+                                                            [0, 2*np.pi / 3, 4 * np.pi / 3]]),
+                                    combine_bands_mode="fairSIM",
+                                    fmax_exclude_band0=0.4,
+                                    normalize_histograms=False,
+                                    background=100,
+                                    gain=2,
+                                    use_gpu=True)
+            
+            # this included parameter estimation
+            imgset.reconstruct()
+            # extract estimated parameters
+            self.mcSIMfrqs = imgset.frqs
+            self.mcSIMphases = imgset.phases - np.expand_dims(imgset.phase_corrections, axis=1)
+            self.mcSIMmod_depths = imgset.mod_depths
+            self.mcSIMotf = imgset.otf
+
+            # clear GPU memory
+            imgset.delete()
+
+                                
 
 
     def reconstruct(self, currentImage):
         '''
         reconstruction
         '''
-        assert self.isCalibrated, 'SIM processor not calibrated, unable to perform SIM reconstruction'
-        dshape= currentImage.shape
-        phases_angles = self.phases_number*self.angles_number
-        rdata = currentImage.reshape(phases_angles, dshape[-2],dshape[-1])
-        if self.use_torch:
-            imageSIM = self.h.reconstruct_pytorch(rdata.astype(np.float32)) #TODO:this is left after conversion from torch
-        else:
-            imageSIM = self.h.reconstruct_rfftw(rdata)
+        if self.reconstructionMethod == "napari":
+            # we use the napari reconstruction method    
+            assert self.isCalibrated, 'SIM processor not calibrated, unable to perform SIM reconstruction'
+            dshape= currentImage.shape
+            phases_angles = self.phases_number*self.angles_number
+            rdata = currentImage.reshape(phases_angles, dshape[-2],dshape[-1])
+            if self.use_torch:
+                imageSIM = self.h.reconstruct_pytorch(rdata.astype(np.float32)) #TODO:this is left after conversion from torch
+            else:
+                imageSIM = self.h.reconstruct_rfftw(rdata)
 
-        return imageSIM
+            return imageSIM
+        
+        elif self.reconstructionMethod == "mcSIM":
+            """
+            test running SIM reconstruction at full speed on GPU
+            """
+
+            '''
+            # load images
+            root_dir = os.path.join(Path(__file__).resolve().parent, 'data')
+            fname_data = os.path.join(root_dir, "synthetic_microtubules_512.tif")
+            imgs = tifffile.imread(fname_data)
+            '''
+
+
+            imgset_next = sim.SimImageSet({"pixel_size": self.dxy,
+                                        "na": self.na,
+                                        "wavelength": self.wavelength},
+                                        currentImage,
+                                        otf=self.mcSIMotf,
+                                        wiener_parameter=0.3,
+                                        frq_estimation_mode="fixed",
+                                        frq_guess=self.mcSIMfrqs,
+                                        phase_estimation_mode="fixed",
+                                        phases_guess=self.mcSIMphases,
+                                        combine_bands_mode="fairSIM",
+                                        mod_depths_guess=self.mcSIMmod_depths,
+                                        use_fixed_mod_depths=True,
+                                        fmax_exclude_band0=0.4,
+                                        normalize_histograms=False,
+                                        background=100,
+                                        gain=2,
+                                        use_gpu=True,
+                                        print_to_terminal=False)
+
+            imgset_next.reconstruct()
+            imageSIM = imgset_next.sim_sr.compute()
+            return imageSIM
 
     def simSimulator(self, Nx=512, Ny=512, Nrot=3, Nphi=3):
         Isample = np.zeros((Nx,Ny))
         Isample[np.random.random(Isample.shape)>0.999]=1
-
 
         allImages = []
         for iRot in range(Nrot):

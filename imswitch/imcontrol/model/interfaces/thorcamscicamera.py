6d939efbd10ab165ce7782414b3562c028c0896d
beniroquai <bene.d@gmx.de>
2023-03-10 22:58:52 +0100

    Adding Thorlabs Scientific camera

---
+++ imswitch/imcontrol/model/interfaces/thorcamscicamera.py
@@ -0,0 +1,387 @@
+from logging import raiseExceptions
+import numpy as np
+import time
+import cv2
+import os
+import sys
+import pathlib
+from imswitch.imcommon.model import initLogger
+import collections
+
+def configure_path():
+    absolute_path_to_dlls = str(pathlib.Path(__file__).resolve().parent)+"\\thorlabs_tsi_sdk\\dll\\"
+    os.environ['PATH'] = absolute_path_to_dlls + os.pathsep + os.environ['PATH']
+
+    try:
+        # Python 3.8 introduces a new method to specify dll directory
+        os.add_dll_directory(absolute_path_to_dlls)
+    except AttributeError:
+        pass
+
+configure_path()
+
+from thorlabs_tsi_sdk.tl_camera import TLCameraSDK
+from thorlabs_tsi_sdk.tl_mono_to_color_processor import MonoToColorProcessorSDK
+from thorlabs_tsi_sdk.tl_camera_enums import SENSOR_TYPE
+
+TAG_BITDEPTH = 32768
+TAG_EXPOSURE = 32769
+
+class CameraThorCamSci:
+    def __init__(self,cameraNo=None, exposure_time = 10000, gain = 0, frame_rate=-1, blacklevel=100, binning=1):
+        super().__init__()
+        self.__logger = initLogger(self, tryInheritParent=True)
+
+        # many to be purged
+        self.model = "CameraThorCamSci"
+        self.shape = (0, 0)
+        
+        # set some flags        
+        self.is_connected = False
+        self.is_streaming = False
+
+        # camera parameters
+        self.blacklevel = blacklevel
+        self.exposure_time = exposure_time
+        self.gain = gain
+        self.preview_width = 600
+        self.preview_height = 600
+        self.frame_rate = frame_rate 
+        self.cameraNo = cameraNo
+
+        # reserve some space for the software-based framebuffer
+        self.NBuffer = 200
+        self.frame_buffer = collections.deque(maxlen=self.NBuffer)
+        self.frameid_buffer = collections.deque(maxlen=self.NBuffer)
+        self.lastFrameId = -1
+                
+        #%% starting the camera thread
+        self.camera = None
+
+        # binning 
+        self.binning = binning
+
+        # connect to camera using the brand-specific python interface
+        dev_num, dev_info_list = self.device_manager.update_device_list()
+
+        if dev_num  != 0:
+            self._init_cam(cameraNo=self.cameraNo, callback_fct=self.set_frame)
+        else:
+            raise Exception("No camera ThorCamSci connected")
+        
+
+        
+
+    def _init_cam(self, cameraNo=1, callback_fct=None):
+        # start camera
+        self.is_connected = True
+        
+        # open the first device
+        self.sdk = TLCameraSDK()
+        cameras = self.sdk.discover_available_cameras()
+        self.camera = cameras[cameraNo]
+        
+        self.camera.frames_per_trigger_zero_for_unlimited = 0
+        self.camera.image_poll_timeout_ms = 2000  # 2 second timeout
+        self.camera.arm(2)
+
+        # save these values to place in our custom TIFF tags later
+        bit_depth = self.camera.bit_depth
+        self.camera.
+        exposure = self.camera.exposure_time_us
+
+        # need to save the image width and height for color processing
+        image_width = self.camera.image_width_pixels
+        image_height = self.camera.image_height_pixels
+
+        # initialize a mono to color processor if this is a color self.camera
+        is_color_camera = (self.camera.camera_sensor_type == SENSOR_TYPE.BAYER)
+        mono_to_color_sdk = None
+        mono_to_color_processor = None
+        if is_color_camera:
+            mono_to_color_sdk = MonoToColorProcessorSDK()
+            mono_to_color_processor = mono_to_color_sdk.create_mono_to_color_processor(
+                self.camera.self.camera_sensor_type,
+                self.camera.color_filter_array_phase,
+                self.camera.get_color_correction_matrix(),
+                self.camera.get_default_white_balance_matrix(),
+                self.camera.bit_depth
+            )
+
+
+        '''
+        # set exposure
+        self.camera.ExposureTime.set(self.exposure_time)
+
+        # set gain
+        self.camera.Gain.set(self.gain)
+        
+        # set framerate
+        self.set_frame_rate(self.frame_rate)
+        
+        # set blacklevel
+        self.camera.BlackLevel.set(self.blacklevel)
+
+        # set the acq buffer count
+        self.camera.data_stream[0].set_acquisition_buffer_number(1)
+        
+        # set camera to mono12 mode
+        try:
+            self.camera.PixelFormat.set(gx.GxPixelFormatEntry.MONO10)
+        # set camera to mono8 mode
+        except:
+            self.camera.PixelFormat.set(gx.GxPixelFormatEntry.MONO8)
+
+        # get framesize 
+        self.SensorHeight = self.camera.HeightMax.get()//self.binning
+        self.SensorWidth = self.camera.WidthMax.get()//self.binning
+        
+        # register the frame callback
+        user_param = None
+        self.camera.register_capture_callback(user_param, callback_fct)
+        '''
+        
+    def start_live(self):
+        if not self.is_streaming:
+            # start data acquisition
+            self.is_streaming = True
+            pass
+            self.camera.stream_on()
+            
+
+    def stop_live(self):
+        if self.is_streaming:
+            # start data acquisition
+            self.is_streaming = False
+            pass
+            self.camera.stream_off()
+
+    def suspend_live(self):
+        if self.is_streaming:
+        # start data acquisition
+            try:
+                pass
+                self.camera.stream_off()
+            except:
+                # camera was disconnected? 
+                pass
+
+            self.is_streaming = False
+        
+    def prepare_live(self):
+        pass
+
+    def close(self):
+        self.camera.disarm()
+        
+    def set_exposure_time(self,exposure_time):
+        self.exposure_time = exposure_time
+        pass
+        self.camera.ExposureTime.set(self.exposure_time*1000)
+
+    def set_gain(self,gain):
+        self.gain = gain
+        pass
+        self.camera.Gain.set(self.gain)
+
+    def set_frame_rate(self, frame_rate):
+        if frame_rate == -1:
+            frame_rate = 10000 # go as fast as you can
+        self.frame_rate = frame_rate
+        
+        # temporary
+        pass
+        
+    def set_blacklevel(self,blacklevel):
+        self.blacklevel = blacklevel
+        pass
+        self.camera.BlackLevel.set(self.blacklevel)
+
+    def set_pixel_format(self,format):
+        pass
+        if self.camera.PixelFormat.is_implemented() and self.camera.PixelFormat.is_writable():
+            if format == 'MONO8':
+                self.camera.PixelFormat.set(gx.GxPixelFormatEntry.MONO8)
+            if format == 'MONO12':
+                self.camera.PixelFormat.set(gx.GxPixelFormatEntry.MONO12)
+            if format == 'MONO14':
+                self.camera.PixelFormat.set(gx.GxPixelFormatEntry.MONO14)
+            if format == 'MONO16':
+                self.camera.PixelFormat.set(gx.GxPixelFormatEntry.MONO16)
+            if format == 'BAYER_RG8':
+                self.camera.PixelFormat.set(gx.GxPixelFormatEntry.BAYER_RG8)
+            if format == 'BAYER_RG12':
+                self.camera.PixelFormat.set(gx.GxPixelFormatEntry.BAYER_RG12)
+        else:
+            print("pixel format is not implemented or not writable")
+
+    def setBinning(self, binning=1):
+        # Unfortunately this does not work
+        # self.camera.BinningHorizontal.set(binning)
+        # self.camera.BinningVertical.set(binning)
+        self.binning = binning
+
+    def getLast(self, is_resize=True):
+        # get frame and save
+#        frame_norm = cv2.normalize(self.frame, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)       
+        #TODO: Napari only displays 8Bit?
+        
+        # only return fresh frames
+        self.frame = self.camera.get_pending_frame_or_null()
+        pass
+        if not self.lastFrameId == self.frameNumber:    
+            self.lastFrameId = self.frameNumber 
+            return self.frame
+
+    def flushBuffer(self):
+        pass
+        self.frameid_buffer.clear()
+        self.frame_buffer.clear()
+        
+    def getLastChunk(self):
+        pass
+        chunk = np.array(self.frame_buffer)
+        frameids = np.array(self.frameid_buffer)
+        self.flushBuffer()
+        self.__logger.debug("Buffer: "+str(chunk.shape)+" IDs: " + str(frameids))
+        return chunk
+    
+    def setROI(self,hpos=None,vpos=None,hsize=None,vsize=None):
+        #hsize = max(hsize, 25)*10  # minimum ROI size
+        #vsize = max(vsize, 3)*10  # minimum ROI size
+        pass
+        hpos = self.camera.OffsetX.get_range()["inc"]*((hpos)//self.camera.OffsetX.get_range()["inc"])
+        vpos = self.camera.OffsetY.get_range()["inc"]*((vpos)//self.camera.OffsetY.get_range()["inc"])  
+        hsize = int(np.min((self.camera.Width.get_range()["inc"]*((hsize*self.binning)//self.camera.Width.get_range()["inc"]),self.camera.WidthMax.get())))
+        vsize = int(np.min((self.camera.Height.get_range()["inc"]*((vsize*self.binning)//self.camera.Height.get_range()["inc"]),self.camera.HeightMax.get())))
+
+        if vsize is not None:
+            self.ROI_width = hsize
+            # update the camera setting
+            if self.camera.Width.is_implemented() and self.camera.Width.is_writable():
+                message = self.camera.Width.set(self.ROI_width)
+                self.__logger.debug(message)
+            else:
+                self.__logger.debug("OffsetX is not implemented or not writable")
+
+        if hsize is not None:
+            self.ROI_height = vsize
+            # update the camera setting
+            if self.camera.Height.is_implemented() and self.camera.Height.is_writable():
+                message = self.camera.Height.set(self.ROI_height)
+                self.__logger.debug(message)
+            else:
+                self.__logger.debug("Height is not implemented or not writable")
+
+        if hpos is not None:
+            self.ROI_hpos = hpos
+            # update the camera setting
+            if self.camera.OffsetX.is_implemented() and self.camera.OffsetX.is_writable():
+                message = self.camera.OffsetX.set(self.ROI_hpos)
+                self.__logger.debug(message)
+            else:
+                self.__logger.debug("OffsetX is not implemented or not writable")
+
+        if vpos is not None:
+            self.ROI_vpos = vpos
+            # update the camera setting
+            if self.camera.OffsetY.is_implemented() and self.camera.OffsetY.is_writable():
+                message = self.camera.OffsetY.set(self.ROI_vpos)
+                self.__logger.debug(message)
+            else:
+                self.__logger.debug("OffsetX is not implemented or not writable")
+
+        return hpos,vpos,hsize,vsize
+
+
+    def setPropertyValue(self, property_name, property_value):
+        # Check if the property exists.
+        if property_name == "gain":
+            self.set_gain(property_value)
+        elif property_name == "exposure":
+            self.set_exposure_time(property_value)
+        elif property_name == "blacklevel":
+            self.set_blacklevel(property_value)
+        elif property_name == "roi_size":
+            self.roi_size = property_value
+        elif property_name == "frame_rate":
+            self.set_frame_rate(property_value)
+        elif property_name == "trigger_source":
+            self.setTriggerSource(property_value)
+        else:
+            self.__logger.warning(f'Property {property_name} does not exist')
+            return False
+        return property_value
+
+    def getPropertyValue(self, property_name):
+        # Check if the property exists.
+        if property_name == "gain":
+            property_value = self.camera.Gain.get()
+        elif property_name == "exposure":
+            property_value = self.camera.ExposureTime.get()
+        elif property_name == "blacklevel":
+            property_value = self.camera.BlackLevel.get()            
+        elif property_name == "image_width":
+            property_value = self.camera.Width.get()//self.binning         
+        elif property_name == "image_height":
+            property_value = self.camera.Height.get()//self.binning
+        elif property_name == "roi_size":
+            property_value = self.roi_size 
+        elif property_name == "frame_Rate":
+            property_value = self.frame_rate 
+        elif property_name == "trigger_source":
+            property_value = self.trigger_source
+        else:
+            self.__logger.warning(f'Property {property_name} does not exist')
+            return False
+        return property_value
+
+    def setTriggerSource(self, trigger_source):
+        pass
+            
+    def getFrameNumber(self):
+        return self.frameNumber 
+
+    def send_trigger(self):
+        pass
+
+    def openPropertiesGUI(self):
+        pass
+    
+    def set_frame(self, user_param, frame):
+        pass
+        if frame is None:
+            self.__logger.error("Getting image failed.")
+            return
+        if frame.get_status() != 0:
+            self.__logger.error("Got an incomplete frame")
+            return
+        numpy_image = frame.get_numpy_array()
+        if numpy_image is None:
+            return
+        self.frame = numpy_image
+        self.frameNumber = frame.get_frame_id()
+        self.timestamp = time.time()
+        
+        if self.binning > 1:
+            numpy_image = cv2.resize(numpy_image, dsize=None, fx=1/self.binning, fy=1/self.binning, interpolation=cv2.INTER_AREA)
+    
+        self.frame_buffer.append(numpy_image)
+        self.frameid_buffer.append(self.frameNumber)
+    
+
+# Copyright (C) ImSwitch developers 2021
+# This file is part of ImSwitch.
+#
+# ImSwitch is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ImSwitch is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.    
---
+++ imswitch/imcontrol/model/managers/detectors/ThorCamSciManager.py
@@ -0,0 +1,281 @@
+import numpy as np
+
+from imswitch.imcommon.model import initLogger
+from .DetectorManager import DetectorManager, DetectorAction, DetectorNumberParameter, DetectorListParameter
+
+
+class ThorCamSciManager(DetectorManager):
+    """ DetectorManager that deals with TheImagingSource cameras and the
+    parameters for frame extraction from them.
+
+    Manager properties:
+    """
+
+    def __init__(self, detectorInfo, name, **_lowLevelManagers):
+        self.__logger = initLogger(self, instanceName=name)
+        
+        # containing all the information from the json file
+        self.detectorInfo = detectorInfo
+
+        # cast the json-parsed values to the correct type
+        binning = detectorInfo.managerProperties['thorcamscicam']["binning"]
+        cameraId = detectorInfo.managerProperties['cameraListIndex']
+        try:
+            pixelSize = detectorInfo.managerProperties['cameraEffPixelsize'] # mum
+        except:
+            # returning back to default pixelsize
+            pixelSize = 1
+        
+        # Get camera object (either mock or real)
+        self._camera = self._getGXObj(cameraId, binning)
+        
+        # assign the properties from the json file to the camera
+        for propertyName, propertyValue in detectorInfo.managerProperties['thorcamscicam'].items():
+            self._camera.setPropertyValue(propertyName, propertyValue)
+
+        # set the shape (important for the recording manager)
+        fullShape = (self._camera.SensorWidth, 
+                     self._camera.SensorHeight)
+        
+        # set the model
+        model = self._camera.model
+        
+        # prepare the parameters
+        self._running = False
+        self._adjustingParameters = False
+
+        # eventually crop the image using stored values (most likely needs the acqusition process to pause)
+        self.crop(hpos=0, vpos=0, hsize=fullShape[0], vsize=fullShape[1])
+
+
+        # Prepare parameters for the GUI
+        parameters = {
+            'exposure': DetectorNumberParameter(group='Misc', value=100, valueUnits='ms',
+                                                editable=True),
+            'gain': DetectorNumberParameter(group='Misc', value=1, valueUnits='arb.u.',
+                                            editable=True),
+            'blacklevel': DetectorNumberParameter(group='Misc', value=100, valueUnits='arb.u.',
+                                            editable=True),
+            'image_width': DetectorNumberParameter(group='Misc', value=fullShape[0], valueUnits='arb.u.',
+                        editable=False),
+            'image_height': DetectorNumberParameter(group='Misc', value=fullShape[1], valueUnits='arb.u.',
+                        editable=False),
+            'frame_rate': DetectorNumberParameter(group='Misc', value=-1, valueUnits='fps',
+                                    editable=True),
+            'trigger_source': DetectorListParameter(group='Acquisition mode',
+                            value='Continous',
+                            options=['Continous',
+                                        'Internal trigger',
+                                        'External trigger'],
+                            editable=True), 
+            'Camera pixel size': DetectorNumberParameter(group='Miscellaneous', value=pixelSize,
+                                                valueUnits='µm', editable=True)
+            }            
+
+        # Prepare actions
+        actions = {
+            'More properties': DetectorAction(group='Misc',
+                                              func=self._camera.openPropertiesGUI)
+        }
+
+        # Initialize DetectorManager
+        super().__init__(detectorInfo, name, fullShape=fullShape, supportedBinnings=[1],
+                         model=model, parameters=parameters, actions=actions, croppable=True)
+        
+
+    def _updatePropertiesFromCamera(self):
+        self.setParameter('Real exposure time', self._camera.getPropertyValue('exposure_time')[0])
+        self.setParameter('Internal frame interval',
+                          self._camera.getPropertyValue('internal_frame_interval')[0])
+        self.setParameter('Readout time', self._camera.getPropertyValue('timing_readout_time')[0])
+        self.setParameter('Internal frame rate',
+                          self._camera.getPropertyValue('internal_frame_rate')[0])
+
+        triggerSource = self._camera.getPropertyValue('trigger_source')
+        if triggerSource == 1:
+            self.setParameter('Trigger source', 'Internal trigger')
+        else:
+            triggerMode = self._camera.getPropertyValue('trigger_mode')
+            if triggerSource == 2 and triggerMode == 6:
+                self.setParameter('Trigger source', 'External "start-trigger"')
+            elif triggerSource == 2 and triggerMode == 1:
+                self.setParameter('Trigger source', 'External "frame-trigger"')
+
+    
+    
+    def getLatestFrame(self, is_save=False):
+        """this function waits for the latest frame from the camera and returns it"""
+        return self._camera.getLast()
+
+
+    def setParameter(self, name, value):
+        """Sets a parameter value and returns the value.
+        If the parameter doesn't exist, i.e. the parameters field doesn't
+        contain a key with the specified parameter name, an error will be
+        raised."""
+
+        super().setParameter(name, value)
+
+        if name not in self._DetectorManager__parameters:
+            raise AttributeError(f'Non-existent parameter "{name}" specified')
+
+        value = self._camera.setPropertyValue(name, value)
+        return value
+
+    def getParameter(self, name):
+        """Gets a parameter value and returns the value.
+        If the parameter doesn't exist, i.e. the parameters field doesn't
+        contain a key with the specified parameter name, an error will be
+        raised."""
+
+        if name not in self._parameters:
+            raise AttributeError(f'Non-existent parameter "{name}" specified')
+
+        value = self._camera.getPropertyValue(name)
+        return value
+
+
+    def setTriggerSource(self, source):
+        """Sets the trigger source and returns the value."""
+        if source == 'Continous':
+            self._performSafeCameraAction(
+                lambda: self._camera.setPropertyValue('trigger_source', 0)
+            )
+        elif source == 'Internal trigger':
+            self._performSafeCameraAction(
+                lambda: self._camera.setPropertyValue('trigger_source', 1)
+            )
+        elif source == 'External trigger':
+            self._performSafeCameraAction(
+                lambda: self._camera.setPropertyValue('trigger_source', 2)
+            )
+        else:
+            raise ValueError(f'Invalid trigger source "{source}"')
+
+        
+    def getChunk(self):
+        """Get the latest chunk/buffer from the camera. Can be software-based queue or hardware-based buffer."""
+        try:
+            return self._camera.getLastChunk()
+        except:
+            return None
+
+    def flushBuffers(self):
+        """Remove all the buffers from the camera's buffer queue."""
+        self._camera.flushBuffer()
+
+    def startAcquisition(self):
+        """Starts the acquisition process."""
+        if self._camera.model == "mock":
+            self.__logger.debug('We could attempt to reconnect the camera')
+            pass
+            
+        if not self._running:
+            self._camera.start_live()
+            self._running = True
+            self.__logger.debug('startlive')
+
+    def stopAcquisition(self):
+        """Stop the acquisition process."""
+        if self._running:
+            self._running = False
+            self._camera.suspend_live()
+            self.__logger.debug('suspendlive')
+
+    def stopAcquisitionForROIChange(self):
+        """Pause the acquisition process for ROI change."""
+        self._running = False
+        self._camera.stop_live()
+        self.__logger.debug('stoplive')
+
+    def finalize(self) -> None:
+        """Safe disconnect of the camera."""
+        super().finalize()
+        self.__logger.debug('Safely disconnecting the camera...')
+        self._camera.close()
+
+    @property
+    def pixelSizeUm(self):
+        """set the camera pixel size in um"""
+        umxpx = self.parameters['Camera pixel size'].value
+        return [1, umxpx, umxpx]
+
+    def setPixelSizeUm(self, pixelSizeUm):
+        """Set the camera pixel size in um"""
+        self.parameters['Camera pixel size'].value = pixelSizeUm
+
+    def crop(self, hpos, vpos, hsize, vsize):
+        """Crop the camera frame to the specified size and position. This is a parameter carried on the camera"""
+        def cropAction():
+            self.__logger.debug(
+                f'{self._camera.model}: crop frame to {hsize}x{vsize} at {hpos},{vpos}.'
+            )
+            self._camera.setROI(hpos, vpos, hsize, vsize)
+            # TOdO: weird hackaround
+            self._shape = (self._camera.camera.Width.get()//self._camera.binning, self._camera.camera.Height.get()//self._camera.binning)
+            self._frameStart = (hpos, vpos)
+            pass
+        try:
+            self._performSafeCameraAction(cropAction)
+        except Exception as e:
+            self.__logger.error(e)
+            # TODO: unsure if frameStart is needed? Try without.
+        # This should be the only place where self.frameStart is changed
+        
+        # Only place self.shapes is changed
+        
+        pass 
+
+    def _performSafeCameraAction(self, function):
+        """ This method is used to change those camera properties that need
+        the camera to be idle to be able to be adjusted.
+        """
+        self._adjustingParameters = True
+        wasrunning = self._running
+        self.stopAcquisitionForROIChange()
+        function()
+        if wasrunning:
+            self.startAcquisition()
+        self._adjustingParameters = False
+
+    def openPropertiesDialog(self):
+        self._camera.openPropertiesGUI()
+
+    def _getGXObj(self, cameraId, binning=1):
+        """Load the camera object from the cameraId and if it fails, load the MOCK"""
+        try:
+            from imswitch.imcontrol.model.interfaces.thorcamscicamera import CameraThorCamSci
+            self.__logger.debug(f'Trying to initialize Daheng Imaging camera {cameraId}')
+            camera = CameraThorCamSci(cameraNo=cameraId, binning=binning)
+        except Exception as e:
+            self.__logger.debug(e)
+            self.__logger.warning(f'Failed to initialize CameraThorCamSci {cameraId}, loading TIS mocker')
+            from imswitch.imcontrol.model.interfaces.tiscamera_mock import MockCameraTIS
+            camera = MockCameraTIS()
+
+        self.__logger.info(f'Initialized camera, model: {camera.model}')
+        return camera
+    
+    def getFrameNumber(self):
+        """Get the frame number of the camera"""
+        return self._camera.getFrameNumber()
+
+    def closeEvent(self):
+        """Close and disconnect the camera safely"""
+        self._camera.close()
+
+# Copyright (C) ImSwitch developers 2021
+# This file is part of ImSwitch.
+#
+# ImSwitch is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ImSwitch is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.

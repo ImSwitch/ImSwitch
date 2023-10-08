6025c07a0ad9aafd99e6d156c2609acb7f0487ba
Jacopo Abramo <jacopo.abramo@gmail.com>
2023-10-08 12:12:33 +0200

    Adding `PositionTableDialog` class
    Based on the `BetterTableWidget` class

--- imswitch/imcommon/view/guitools/dialogtools.py
+++ imswitch/imcommon/view/guitools/dialogtools.py
@@ -1,5 +1,12 @@
+from json import dump
+from typing import Union
+from .BetterTableWidget import BetterTableWidget
+from .BetterPushButton import BetterPushButton
 from qtpy import QtCore, QtWidgets
-
+from qtpy.QtWidgets import (
+    QDialog,
+    QGridLayout,
+)
 
 def askYesNoQuestion(widget, title, question):
     """ Asks the user a yes/no question and returns whether "yes" was clicked. """
@@ -33,6 +40,121 @@ def askForFolderPath(widget, caption=None, defaultFolder=None):
     result = QtWidgets.QFileDialog.getExistingDirectory(widget, caption=caption,
                                                         directory=defaultFolder)
     return result if result else None
+
+class PositionsTableDialog(QDialog):
+    def __init__(self, title: str, coordinates: list, default: Union[str, int, float],  unit: str = "µm", labelName: str = "Point"):
+        super(QDialog, self).__init__()
+        
+        self.coordinates = coordinates
+        
+        # Create the dialog
+        self.resize(600, 400)  # Set the size of the dialog.
+        gridLayout = QGridLayout(self)  # Create a grid layout.
+        
+        # Create the table widget
+        self.pointsTableWidget = BetterTableWidget(names=coordinates, default=default, unit=unit, labelName=labelName)
+        
+        numColumns = len(coordinates) + 1
+        
+        # Create the add row button
+        self.addPosButton = BetterPushButton(self)
+        self.addPosButton.setText("Add position")  # Set label of the button.
+        self.addPosButton.clicked.connect(self.pointsTableWidget.addNewRow)  # Connect the button to a function.
+        
+        # Create the remove row button
+        self.removePosButton = BetterPushButton(self)
+        self.removePosButton.setText("Remove position")  # Set label of the button.
+        self.removePosButton.clicked.connect(self.pointsTableWidget.removeSelectedRow)  # Connect the button to a function.
+        
+        # The remove row button is active only when a row is selected
+        self.removePosButton.setEnabled(False)
+        self.pointsTableWidget.selectionModel().selectionChanged.connect(
+            self.setRemoveButtonStatus
+        )
+        
+        # Create the save table button
+        self.saveTableButton = BetterPushButton(self)
+        self.saveTableButton.setText("Save table")  # Set label of the button.
+        self.saveTableButton.clicked.connect(self.saveTable)
+        
+        # Create OK and Cancel buttons
+        self.okButton = BetterPushButton(self)
+        self.okButton.setText("OK")
+        self.okButton.clicked.connect(self.accept)
+        self.cancelbutton = BetterPushButton(self)
+        self.cancelbutton.setText("Cancel")
+        self.cancelbutton.clicked.connect(self.reject)
+        
+        
+        gridLayout.addWidget(self.pointsTableWidget, 0, 0, 6, numColumns)
+        gridLayout.addWidget(self.addPosButton, 0, numColumns)
+        gridLayout.addWidget(self.removePosButton, 1, numColumns)
+        gridLayout.addWidget(self.saveTableButton, 2, numColumns)
+        gridLayout.addWidget(self.okButton, 4, numColumns)
+        gridLayout.addWidget(self.cancelbutton, 5, numColumns)
+        self.setLayout(gridLayout)
+        self.setWindowTitle(title)
+    
+    def __del__(self):
+        # TODO: if there are connected signals, disconnect them
+        pass
+    
+    def accept(self) -> None:
+        # TODO: add a signal to send the table data to the main caller
+        super().accept()
+    
+    def setRemoveButtonStatus(self, _):
+        selectedItems = len(self.pointsTableWidget.selectedItems())
+        self.removePosButton.setEnabled(selectedItems != 0)
+
+    def saveTable(self):
+        """ Saves the table as a CSV or JSON file. A file explorer window will open,
+        and the user will be asked to pick a file path.
+        """
+        fileFilter = "JSON (*.json);;CSV (*.csv)"
+        filePath: str = askForFilePath(self, caption="Save table", isSaving=True, nameFilter=fileFilter)
+        if filePath is not None:
+            if filePath.endswith(".csv"):
+                self._saveTableAsCSV(filePath)
+            elif filePath.endswith(".json"):
+                self._saveTableAsJSON(filePath)
+    
+    def _saveTableAsCSV(self, filePath: str):
+        with open(filePath, "w") as f:
+            # Write the data
+            header = self.coordinates
+            if self.pointsTableWidget.labelName is not None:
+                header.append("Label")
+            f.write(",".join(header) + "\n")
+            for row in range(self.pointsTableWidget.rowCount()):
+                rowData = self._parseRowElements(row)
+                rowData = [str(item) for item in rowData]
+                f.write(",".join(rowData) + "\n")
+    
+    def _saveTableAsJSON(self, filePath: str):
+        data = {}
+        keys = self.coordinates
+        if self.pointsTableWidget.labelName is not None:
+            keys.append("Label")
+        for row in range(self.pointsTableWidget.rowCount()):
+            rowData = self._parseRowElements(row)
+            data[row] = {key: value for key, value in zip(keys, rowData)}
+        
+        with open(filePath, "w") as f:
+            dump(data, f, indent=4)
+
+    def _parseRowElements(self, row: int):
+        """ Parses the elements of a row and returns them as a list.
+        """
+        table = self.pointsTableWidget
+        rowData = []
+        if type(table.default) in [int, float]:
+            if table.labelName is not None:
+                rowData = [table.cellWidget(row, col).value() for col in range(table.columnCount()-1)]
+                rowData.append(table.item(row, table.columnCount()-1).text())
+            else:
+                rowData = [table.itemAt(row, col).text() for col in range(table.columnCount())]
+        return rowData
 
 
 # Copyright (C) 2020-2021 ImSwitch developers

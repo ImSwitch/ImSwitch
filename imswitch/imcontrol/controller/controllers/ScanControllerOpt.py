852e5a891dfc118bf2582d9bbd33fd089b884279
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-03-01 11:03:14 +0100

    Update copyright

--- imswitch/imcontrol/controller/controllers/ScanControllerOpt.py
+++ imswitch/imcontrol/controller/controllers/ScanControllerOpt.py
@@ -1202,7 +1202,7 @@ def _array_to_params(array):
         out.append(tmp)
     return out
 
-# Copyright (C) 2020-2021 ImSwitch developers
+# Copyright (C) 2020-2024 ImSwitch developers
 # This file is part of ImSwitch.
 #
 # ImSwitch is free software: you can redistribute it and/or modify

346a218af461a9de3525b57fe75915d0d12701a3
beniroquai <bene.d@gmx.de>
2024-03-07 12:33:43 +0100

    Updating ZMQ implementation to work asynchrounly

--- tools/SIMPatternServer/pygame_display.py
+++ tools/SIMPatternServer/pygame_display.py
@@ -11,7 +11,7 @@ from fastapi import FastAPI, BackgroundTasks
 import uvicorn
 
 ZMQPORT = 5555
-IS_FULLSCREEN = False
+
 # Part of the FastAPI application
 
 class ViewerController:
@@ -19,16 +19,12 @@ class ViewerController:
         self.context = zmq.Context()
         # Use REQ socket for request-reply pattern
         self.socket = self.context.socket(zmq.PAIR)
-        self.socket.bind("tcp://*:"+str(ZMQPORT))
+        self.socket.connect("tcp://localhost:"+str(ZMQPORT))
     
     def send_command(self, command):
-        self.send_command_and_receive(command)
+        self.socket.send_string(command)
         
     def send_command_and_receive(self, command):
-        # clear messages in the socket
-        while self.socket.poll(0):
-            print(self.socket.recv_string(zmq.NOBLOCK))
-        
         self.socket.send_string(command)
         # Block until a reply is received
         message = self.socket.recv_string()
@@ -39,9 +35,8 @@ class ViewerController:
 
     def change_wavelength(self, wavelength):
         self.send_command(f"change_wavelength:{wavelength}")
-        
-    def set_wait_time(self, tWait):
-        self.send_command(f"set_wait_time:{tWait}")
+
+viewer_controller = ViewerController()
 
 class ImageLoader:
     def __init__(self, path_488, path_635, n_images=9):
@@ -63,8 +58,6 @@ class ImageLoader:
             # Handle error or generate random patterns
         return images
 
-app = FastAPI()
-viewer_controller = ViewerController()
 
 class PygameViewer:
     def __init__(self, display_size, path_488, path_635):
@@ -75,14 +68,11 @@ class PygameViewer:
         self.socket = self.context.socket(zmq.PAIR)
         self.socket.connect("tcp://localhost:"+str(ZMQPORT))
         self.current_images = self.loader.images_488  # Default to 488nm images
-        self.tWait = 0.5  # Default time to wait between patterns
+        self.tWait = 0  # Default time to wait between patterns
     
     def run(self):
         pygame.init()
-        if IS_FULLSCREEN:
-            self.display = pygame.display.set_mode(self.display_size, pygame.FULLSCREEN)
-        else:
-            self.display = pygame.display.set_mode(self.display_size)
+        self.display = pygame.display.set_mode(self.display_size)#, pygame.FULLSCREEN)
         pygame.mouse.set_visible(False)
         self.running = True
         while self.running:
@@ -94,7 +84,7 @@ class PygameViewer:
                 # Non-blocking check for a message from ZMQ
                 try:
                     # Wait for next request from client
-                    message = self.socket.recv_string(zmq.NOBLOCK)
+                    message = self.socket.recv_string()
                     reply = self.handle_message(message)
                     # Send reply back to client
                     self.socket.send_string(reply)
@@ -103,7 +93,7 @@ class PygameViewer:
                     pass  # No message received
 
                 pygame.display.flip()  # Update the full display surface to the screen
-                pygame.time.wait(10)  # Small delay to prevent high CPU usage
+                pygame.time.wait(20)  # Small delay to prevent high CPU usage
     
     def handle_message(self, message):
         response = ""
@@ -118,8 +108,6 @@ class PygameViewer:
         elif message == "start_single_loop":
             self.start_single_loop()
             response = "Current pattern info or any other data"
-        elif message == "set_wait_time":
-            self.set_wait_time(float(message.split(":")[1]))
         return response
     
     def trigger(self):
@@ -148,58 +136,17 @@ class PygameViewer:
             self.display_pattern(i)
             self.trigger()
             time.sleep(self.tWait)
-            
-    def set_wait_time(self, tWait):
-        self.tWait = tWait
         
-@app.get("/display_pattern/{pattern_id}")
-async def display_pattern(pattern_id: int):
-    viewer_controller.display_pattern(pattern_id)
-    return {"message": f"Displaying pattern {pattern_id}."}
-
-@app.get("/start_viewer/")
-async def start_viewer():
-    viewer_controller.send_command("start")
-    return {"message": "Viewer started."}
-
-@app.get("/stop_viewer/")
-async def stop_viewer():
-    viewer_controller.send_command("stop")
-    return {"message": "Viewer stopped."}
-
-
-# Corresponding FastAPI endpoint
-@app.get("/change_wavelength/{wavelength}")
-async def change_wavelength(wavelength: int):
-    viewer_controller.change_wavelength(wavelength)
-    return {"message": f"Wavelength changed to {wavelength}nm."}
-
-@app.get("/start_viewer_single_loop/{i_cycle}")
-async def start_viewer_single_loop(wavelength: int):
-    viewer_controller.change_wavelength(wavelength)
-    response = viewer_controller.send_command_and_receive("start_single_loop")
-    return {"message": "Viewer started in single loop mode."}
-
-@app.get("/set_wait_time/{tWait}")
-async def set_wait_time(tWait: float):
-    viewer_controller.set_wait_time(tWait)
-    return {"message": f"Wait time set to {tWait}."}
-
-# Implement other endpoints similarly, using viewer_controller to send appropriate commands
 
 PATH_488 = "/Users/bene/ImSwitchConfig/imcontrol_sim/488"
 PATH_635 = "/Users/bene/ImSwitchConfig/imcontrol_sim/635"
 DISPLAY_SIZE = (1920, 1080)  
 
-import threading
+
 if __name__ == "__main__":
-    def startServer():
-        uvicorn.run(app, host="0.0.0.0", port=8000)
-    
     def startViewer():
         viewer = PygameViewer(DISPLAY_SIZE, PATH_488, PATH_635)
         viewer.run()        
-    threading.Thread(target=startServer).start()
     startViewer()
     
     

bba91196ca315e4492ef6de0cd3a20f3db15aff9
beniroquai <bene.d@gmx.de>
2024-04-08 07:13:47 +0100

    minor updates from the raspi side

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import os
+
+os.environ["DISPLAY"] = ":0"
+os.environ["QT_QPA_PLATFORM"] = "offscreen"
+
 from imswitch.__main__ import main
-#sudo usermod -a -G dialout bene
-#export DISPLAY=:0
-#export QT_QPA_PLATFORM=offscreen
-import subprocess
-subprocess.run(["export", "DISPLAY=:0"], shell=True)
-subprocess.run(["export", "QT_QPA_PLATFORM=offscreen"], shell=True)
+'''
+sudo usermod -a -G dialout bene
+export DISPLAY=:0
+export QT_QPA_PLATFORM=offscreen
+'''
 
 main()

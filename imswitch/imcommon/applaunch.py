f921862f3881f4f626c38ee984b487f1ffb774e5
beniroquai <bene.d@gmx.de>
2024-07-15 00:10:44 +0200

    Update applaunch.py

--- imswitch/imcommon/applaunch.py
+++ imswitch/imcommon/applaunch.py
@@ -3,6 +3,7 @@ import os
 import sys
 import traceback
 
+from .model import dirtools, pythontools, initLogger
 from imswitch import IS_HEADLESS
 if not IS_HEADLESS:
     from qtpy import QtCore, QtGui, QtWidgets
@@ -10,13 +11,12 @@ if not IS_HEADLESS:
     from PyQt5.QtCore import Qt
     from PyQt5.QtWidgets import QApplication
 
-from .model import dirtools, pythontools, initLogger
 
 def prepareApp():
     """ This function must be called before any views are created. """
-    from PyQt5.QtCore import Qt
-    from PyQt5.QtWidgets import QApplication
-
+    if IS_HEADLESS:
+        """We won't have any GUI, so we don't need to prepare the app."""
+        return None
     # Initialize exception handling
     pythontools.installExceptHook()
 
@@ -51,6 +51,9 @@ def prepareApp():
 
 def launchApp(app, mainView, moduleMainControllers):
     """ Launches the app. The program will exit when the app is exited. """
+    if IS_HEADLESS:
+        """We won't have any GUI, so we don't need to prepare the app."""
+        return None
 
     logger = initLogger('launchApp')
 

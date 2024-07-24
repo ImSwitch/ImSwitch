2788ec7583683f3cd9792042df69440cc55f56c8
Testalab <82811179+GuillaumeMinet@users.noreply.github.com>
2024-07-24 17:14:57 +0200

    Correct errors in previous merge

--- imswitch/imcontrol/controller/controllers/SLMmlController.py
+++ imswitch/imcontrol/controller/controllers/SLMmlController.py
@@ -25,6 +25,7 @@ class SLMmlController(ImConWidgetController):
         self.__slmSize = (self.__slmInfo.width, self.__slmInfo.height)"""
 
         self.slmDir = os.path.join(dirtools.UserFileDirs.Root, 'imcontrol_slm')
+        
         if not os.path.exists(self.slmDir):
             os.makedirs(self.slmDir)
 
@@ -33,7 +34,6 @@ class SLMmlController(ImConWidgetController):
             return
 
         self._widget.initSLMDisplay(self._setupInfo.slm.monitorIdx)
-        self.MFApath = None
 
         # Connect CommunicationChannel signals
         self._commChannel.sigSLMMaskUpdated.connect(lambda mask: self.displayMask(mask))
@@ -76,6 +76,16 @@ class SLMmlController(ImConWidgetController):
 
         # Initial SLM display
         self.displayMask(self._master.slmManager.maskCombined)
+
+
+
+    def updateApplyMFA(self,state:bool):
+        self._master.slmManager.applyMFA = state
+
+    def loadMFA(self):
+        path = guitools.askForFilePath(self._widget, 'Choose MFA hologram BMP image',defaultFolder=self.slmDir)
+        self._widget.updateMLAlabel(Path(path).name)
+        self._master.slmManager.updateMFApath(path)
 
     def toggleSLMDisplay(self, enabled):
         self._widget.setSLMDisplayVisible(enabled)

3cd05874e50261bf383c4955fe07110b7e25fa03
Simone Ezendam <simone.ezendam@scilifelab.se>
2025-11-11 14:44:47 +0100

    Bugfix in AI channel creation of nidaq manager

--- imswitch/imcontrol/model/managers/NidaqManager.py
+++ imswitch/imcontrol/model/managers/NidaqManager.py
@@ -138,18 +138,21 @@ class NidaqManager(SignalInterface):
         #self.__logger.debug(f'Created CO task: {name}')
         return cotask
 
-    def __createChanAITask(self, name, channels, acquisitionType, source, rate,
+    def __createChanAITask(self, name, channel, acquisitionType, source, rate,
                            min_val=-0.5, max_val=10.0, sampsInScan=1000, starttrig=False,
                            reference_trigger='ai/StartTrigger'):
         """ Simplified function to create an analog input task """
         if self.__simulating:
             return None
         aitask = nidaqmx.Task(name)
-        for channel in channels:
-            aitask.ai_channels.add_ai_voltage_chan(channel)
+        #for channel in channels:
+        #    aitask.ai_channels.add_ai_voltage_chan(channel)
+        aitask.ai_channels.add_ai_voltage_chan(channel)
+        if acquisitionType == 'finite':
+            acqType = nidaqmx.constants.AcquisitionType.FINITE
         aitask.timing.cfg_samp_clk_timing(source=source,
                                           rate=rate,
-                                          sample_mode=acquisitionType,
+                                          sample_mode=acqType,
                                           samps_per_chan=sampsInScan)
         if starttrig:
             aitask.triggers.start_trigger.cfg_dig_edge_start_trig(reference_trigger)

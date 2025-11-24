be202120adc19114a08bbc262b497dc0952289a8
Lenny Reinkensmeier <lenny.reinkensmeier@scilifelab.se>
2025-11-24 17:18:32 +0100

    re-added import of legacy SLM Manager for compatibility

--- imswitch/imcontrol/controller/MasterController.py
+++ imswitch/imcontrol/controller/MasterController.py
@@ -1,7 +1,7 @@
 from imswitch.imcommon.model import VFileItem, initLogger
 from imswitch.imcontrol.model import (
-    DetectorsManager, LasersManager, MultiManager, NidaqManager, PositionersManager, RecordingManager, RS232sManager, 
-    ScanManagerPointScan, ScanManagerBase, ScanManagerMoNaLISA, SLMManager, SLMmlManager, StandManager, RotatorsManager
+    DetectorsManager, LasersManager, MultiManager, NidaqManager, PositionersManager, RecordingManager, RS232sManager,
+    ScanManagerPointScan, ScanManagerBase, ScanManagerMoNaLISA, SLMManager, StandManager, RotatorsManager, SLMsManager
 )
 
 
